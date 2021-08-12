84451ba0b0e0bcb5791eea47d77a64f53df6bef0
Puja129 <pujashar9@gmail.com>
2021-08-12 08:17:45 -0400

    Created a Streamlit app to explore various data-analysis and visualization options available in Streamlit using different types of datasets from EC issue #5
    Created a Streamlit app to explore various data-analysis and visualization options available in Streamlit using different types of datasets from EC to address issue #5

---
+++ view_datasets.py
@@ -0,0 +1,169 @@
+#
+#  Copyright (c) 2019 General Electric Company. All rights reserved.
+#
+#  The copyright to the computer software herein is the property of
+#  General Electric Company. The software may be used and/or copied only
+#  with the written permission of General Electric Company or in accordance
+#  with the terms and conditions stipulated in the agreement/contract
+#  under which the software has been supplied.
+#
+#  author: Puja.Sharma@ge.com
+#
+
+
+import os
+import streamlit as st
+
+# EDA Pkgs
+import pandas as pd
+
+# Viz Pkgs
+import matplotlib.pyplot as plt
+import matplotlib
+matplotlib.use('Agg')
+import seaborn as sns
+
+st.set_option('deprecation.showPyplotGlobalUse', False)
+def main():
+
+	html_temp = """
+	<div style="background-color:tomato;"><p style="color:white;font-size:30px;padding:10px">Common ML Dataset Explorer</p></div>
+	"""
+	st.markdown(html_temp,unsafe_allow_html=True)
+
+	def file_selector(folder_path='./datasets'):
+		filenames = os.listdir(folder_path)
+		selected_filename = st.selectbox("Select A file",filenames)
+		return os.path.join(folder_path,selected_filename)
+
+	filename = file_selector()
+	st.info("You Selected {}".format(filename))
+
+	#def get_dataset(dataset_name):
+	if filename == './datasets/browse_history.json':
+		df = pd.read_json(filename).head().T
+	else:
+		df = pd.read_json(filename)
+
+
+	if st.checkbox("Show Dataset"):
+		st.dataframe(df.head())
+
+	# Show Columns
+	if st.button("Column Names"):
+		st.write(df.columns)
+
+	# Show Shape
+	if st.checkbox("Shape of Dataset"):
+		data_dim = st.radio("Show Dimension By ",("Rows","Columns"))
+		if data_dim == 'Rows':
+			st.text("Number of Rows")
+			st.write(df.shape[0])
+		elif data_dim == 'Columns':
+			st.text("Number of Columns")
+			st.write(df.shape[1])
+		else:
+			st.write(df.shape)
+
+	# Select Columns
+	if st.checkbox("Select Columns To Show"):
+		all_columns = df.columns.tolist()
+		selected_columns = st.multiselect("Select",all_columns)
+		new_df = df[selected_columns]
+		st.dataframe(new_df)
+
+	# Show Values
+	if st.button("Value Counts"):
+		st.text("Value Counts By Target/Class")
+		st.write(df.iloc[:,-1].value_counts())
+
+
+	# Show Datatypes
+	if st.button("Data Types"):
+		st.write(df.dtypes)
+
+
+	# Show Summary
+	if st.checkbox("Summary"):
+		st.write(df.describe().T)
+
+	## Plot and Visualization
+
+	st.subheader("Data Visualization")
+	# Correlation
+	# Seaborn Plot
+	if filename == './datasets/browse_history.json':
+		st.text("Map")
+		airports = pd.read_json(filename).head().T[['latitude', 'longitude']][0:100]
+		st.map(airports)
+	else:
+		if st.checkbox("Correlation Plot[Seaborn]"):
+			st.write(sns.heatmap(df.corr(),annot=True))
+			st.pyplot()
+
+
+	# Pie Chart
+	if st.checkbox("Pie Plot"):
+		all_columns_names = df.columns.tolist()
+		if st.button("Generate Pie Plot"):
+			st.success("Generating A Pie Plot")
+			st.write(df.iloc[:,-1].value_counts().plot.pie(autopct="%1.1f%%"))
+			st.pyplot()
+
+	# Count Plot
+	if st.checkbox("Plot of Value Counts"):
+		st.text("Value Counts By Target")
+		all_columns_names = df.columns.tolist()
+		primary_col = st.selectbox("Primary Columm to GroupBy",all_columns_names)
+		selected_columns_names = st.multiselect("Select Columns",all_columns_names)
+		if st.button("Plot"):
+			st.text("Generate Plot")
+			if selected_columns_names:
+				vc_plot = df.groupby(primary_col)[selected_columns_names].count()
+			else:
+				vc_plot = df.iloc[:,-1].value_counts()
+			st.write(vc_plot.plot(kind="bar"))
+			st.pyplot()
+
+
+	# Customizable Plot
+
+	st.subheader("Customizable Plot")
+	all_columns_names = df.columns.tolist()
+	type_of_plot = st.selectbox("Select Type of Plot",["area","bar","line","hist","box","kde"])
+	selected_columns_names = st.multiselect("Select Columns To Plot",all_columns_names)
+
+	if st.button("Generate Plot"):
+		st.success("Generating Customizable Plot of {} for {}".format(type_of_plot,selected_columns_names))
+
+		# Plot By Streamlit
+		if type_of_plot == 'area':
+			cust_data = df[selected_columns_names]
+			st.area_chart(cust_data)
+
+		elif type_of_plot == 'bar':
+			cust_data = df[selected_columns_names]
+			st.bar_chart(cust_data)
+
+		elif type_of_plot == 'line':
+			cust_data = df[selected_columns_names]
+			st.line_chart(cust_data)
+
+		# Custom Plot
+		elif type_of_plot:
+			cust_plot= df[selected_columns_names].plot(kind=type_of_plot)
+			st.write(cust_plot)
+			st.pyplot()
+
+	if st.button("Thanks"):
+		st.balloons()
+
+	st.sidebar.header("About App")
+	st.sidebar.info("This app is designed to perform EDA on EC-data for ML")
+
+	st.sidebar.text("Built with Streamlit")
+	st.sidebar.text("Maintained by Puja Sharma")
+
+
+if __name__ == '__main__':
+	main()
